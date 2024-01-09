a0e11b649f4ef1a7d9618d3831504ef67ee65275
Felipe <felipetermometria@hotmail.com>
2024-01-09 08:41:37 -0300

    Fragment sign

--- app/src/main/java/com/example/messenger/core/authentication/view/LoginActivity.kt
+++ app/src/main/java/com/example/messenger/core/authentication/view/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.messenger.core.authentication.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
 import com.example.messenger.R
 
 interface LoginCallback{
@@ -15,6 +16,10 @@ class LoginActivity : AppCompatActivity(), LoginCallback {
     }
 
     override fun onCreateAccountClicked() {
-
+        val fragment = SignUpFragment()
+        supportFragmentManager.commit {
+            replace(R.id.fragment_container_view, fragment)
+            addToBackStack(null)
+        }
     }
 }
